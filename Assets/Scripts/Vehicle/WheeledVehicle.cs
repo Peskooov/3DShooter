1c5081cdab12d9ccf6bdd02ed9bf9ace4ce07e0f
Peskooov <162593364+Peskooov@users.noreply.github.com>
2024-12-12 04:01:15 +0300

    NP 24

---
+++ Assets/Scripts/Vehicle/WheeledVehicle.cs
@@ -0,0 +1,99 @@
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public class WheelAxle
+{
+    [SerializeField] private WheelCollider leftWheelCollider;
+    [SerializeField] private WheelCollider rightWheelCollider;
+
+    [SerializeField] private Transform leftWheelMesh;
+    [SerializeField] private Transform rightWheelMesh;
+
+    [SerializeField] private bool motor;
+    public bool Motor => motor;
+
+    [SerializeField] private bool steering;
+    public bool Steering => steering;
+
+    public void SetTorque(float torque)
+    {
+        if (!motor) return;
+
+        leftWheelCollider.motorTorque = torque;
+        rightWheelCollider.motorTorque = torque;
+    }
+
+    public void Break(float breakTorque)
+    {
+        leftWheelCollider.brakeTorque = breakTorque;
+        rightWheelCollider.brakeTorque = breakTorque;
+    }
+
+    public void SetSteerAngle(float angle)
+    {
+        if (!steering) return;
+
+        leftWheelCollider.steerAngle = angle;
+        rightWheelCollider.steerAngle = angle;
+    }
+
+    public void UpdateMeshTransform()
+    {
+        UpdateWheelTransform(leftWheelCollider, ref leftWheelMesh);
+        UpdateWheelTransform(rightWheelCollider, ref rightWheelMesh);
+    }
+
+    private void UpdateWheelTransform(WheelCollider wheelCollider, ref Transform wheelTransform)
+    {
+        Vector3 position;
+        Quaternion rotation;
+
+        wheelCollider.GetWorldPose(out position, out rotation);
+        wheelTransform.position = position;
+        wheelTransform.rotation = rotation;
+    }
+}
+
+[RequireComponent(typeof(Rigidbody))]
+public class WheeledVehicle : MonoBehaviour
+{
+    [SerializeField] private WheelAxle[] wheelAxles;
+    [SerializeField] private float maxMotorTorque;
+    [SerializeField] private float breakTorque;
+    [SerializeField] private float maxSteerAngle;
+    [SerializeField] private float maxLinearVelocity;
+
+    private Rigidbody rb;
+
+    public float LinearVelocity => rb.velocity.magnitude;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float targetMotor = maxMotorTorque * Input.GetAxis("Vertical");
+        float brakeTorque = breakTorque * Input.GetAxis("Jump");
+        float steering = maxSteerAngle * Input.GetAxis("Horizontal");
+
+        for (int i = 0; i < wheelAxles.Length; i++)
+        {
+            if (brakeTorque == 0 && LinearVelocity < maxLinearVelocity)
+            {
+                wheelAxles[i].Break(0);
+                wheelAxles[i].SetTorque(targetMotor);
+            }
+
+            if (LinearVelocity > maxLinearVelocity)
+                wheelAxles[i].Break(brakeTorque * 0.2f);
+            else
+                wheelAxles[i].Break(brakeTorque);
+
+            wheelAxles[i].SetSteerAngle(steering);
+            wheelAxles[i].UpdateMeshTransform();
+        }
+    }
+}
